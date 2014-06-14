b88e12ed95f91d715cd1b56898b536fbcb5a0162
Mark Tareshawty <tarebyte@gmail.com>
2014-06-14 15:08:49 -0400

    added specs for definitions

--- spec/classes/font-adobe-sourcecodepro_spec.rb
+++ spec/classes/font-adobe-sourcecodepro_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe 'fonts::adobe::sourcecodepro' do
-
+  it should do
+    include_class('fonts::adobe::definitions::sourcecodepro')
+  end
 end
--- spec/classes/font-adobe-sourcesanspro_spec.rb
+++ spec/classes/font-adobe-sourcesanspro_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe 'fonts::adobe::sourcesanspro' do
-
+  it should do
+    include_class('fonts::adobe::definitions::sourcesanspro')
+  end
 end
